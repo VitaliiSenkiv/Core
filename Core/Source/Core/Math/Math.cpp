15d46b7e99b4bf812e0bce96a5786a168be30d65
NazariiSenkiv <95177352+NazariiSenkiv@users.noreply.github.com>
2023-02-18 11:31:35 +0200

    ADDED: Math sin and cos

--- Core/Source/Core/Math/Math.cpp
+++ Core/Source/Core/Math/Math.cpp
@@ -1,6 +1,5 @@
 #include "Math.h"
 
-#include <cmath>
 #include <ctime>
 
 Math::RandInitializer Math::StaticRandInitializer;
@@ -8,19 +7,4 @@ Math::RandInitializer Math::StaticRandInitializer;
 Math::RandInitializer::RandInitializer()
 {
 	srand(time(nullptr));
-}
-
-float Math::RandRange(float Min, float Max)
-{
-	return Min + (Max - Min) * FRand();
-}
-
-float Math::FRand()
-{
-	return Rand() / RAND_MAX;
-}
-
-int Math::Rand()
-{
-	return rand();
 }
--- Core/Source/Core/Math/Math.h
+++ Core/Source/Core/Math/Math.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <cmath>
+
+constexpr float Pi = 3.1415926535897932f;
+constexpr float DegreesInRadian = 180.f / Pi;
+constexpr float RadiansInDegree = Pi / 180.f;
+
 class Math
 {
 private:
@@ -17,13 +23,19 @@ public:
 	/** Main methods */
 
 	/** Returns rand value in range [Min, Max] */
-	static float RandRange(float Min, float Max);
-
+	static float RandRange(float Min, float Max) { return Min + (Max - Min) * FRand(); }
 	/** Returns randon number in range [0.0, 1.0] */
-	static float FRand();
-
+	static float FRand() { return Rand() / RAND_MAX; }
 	/** Returns random value in range [0, RAND_MAX] */
-	static int Rand();
+	static int Rand() { return rand(); }
+
+	static float RadiansToDegrees(float Radians) { return Radians * DegreesInRadian; }
+	static float DegreesToRadians(float Degrees) { return Degrees * RadiansInDegree; }
+
+	static float Sin(float Radians)		{ return sin(Radians); }
+	static float SinDeg(float Degrees)	{ return Sin(DegreesToRadians(Degrees)); }
+	static float Cos(float Radians)		{ return cos(Radians); }
+	static float CosDeg(float Degrees)	{ return Cos(DegreesToRadians(Degrees));  }
 
 private:
 	/** This object is used to initialize random for Math static class (this is one of possible ways to create static ctors) */
